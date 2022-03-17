d2aab6cd54a640350beecbef870266ee460e10b6
TamDouye <douyeeferusuoa@gmail.com>
2022-03-17 18:36:01 +0100

    C programming

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - a function that adds two integers and returns the result
+ * @pap: first number input
+ * @akara: second number input
+ * Return: pap + akara
+ */
+int add(int pap, int akara)
+{
+return (pap + akara);
+}[A[A[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[A[D[D[D[D[D[D[D[D[D[D[D[D[D[Dgnutgarri[B[C[C[C[Cgnut[C[C[C[C[Cgarri[B[C[C[Cgarri[D[D[D[D[D[D[D[D[D[Dgnut[B[B[Cgarri[D[D[D[D[D[D[Dgnut[B[B[B
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *print_sign - function
+ *@n: variable of the function
+ *Return: Todo depende de la funcion
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+
+else if (n < 0)
+{
+	_putchar('-');
+	return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
+_putchar('\n');
+}
+© 2022 GitHub, Inc.
+Terms
