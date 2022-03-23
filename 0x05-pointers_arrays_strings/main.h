b89bb93a4a03fd537940f4464723e508eb14bbab
TamDouye <douyeeferusuoa@gmail.com>
2022-03-22 21:05:26 -0700

    Pointers and arrays

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,20 +1,39 @@
 #ifndef MAIN_H
+
 #define MAIN_H
 
+
+
 #include <stdio.h>
+
 #include <unistd.h>
 
+
+
 int _putchar(char c);
+
 void reset_to_98(int *n);
+
 void swap_int(int *a, int *b);
+
 int _strlen(char *s);
+
 void _puts(char *str);
+
 void print_rev(char *s);
+
 void rev_string(char *s);
+
 void puts2(char *str);
+
 void puts_half(char *str);
+
 void print_array(int *a, int n);
+
 char *_strcpy(char *dest, char *src);
+
 int _atoi(char *s);
 
-#endif[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[C[C[C[C[C[C[[A[C[C[C[C[C[C[C[C[C[C[C[[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[C[C[C[C[C[C[C[C[D[D
+
+
+#endif
