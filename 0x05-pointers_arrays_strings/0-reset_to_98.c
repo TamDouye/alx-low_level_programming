29f35337a3946364d2b00884f88596aca8d3e54f
TamDouye <douyeeferusuoa@gmail.com>
2022-03-23 02:43:20 +0100

    My A and P

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *reset_to_98
+ *return : void[D[D[D[D[D[D[D[D[D[D[D[R[C[C[C[C[C[C[C[C[C[C[C[C
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
