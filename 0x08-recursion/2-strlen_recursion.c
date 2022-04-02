fe37d0f84028b3eb8298d404348c67414c002565
TamDouye <douyeeferusuoa@gmail.com>
2022-04-02 04:49:49 +0100

    Recursion in C programming

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,33 +1,14 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - main - check the code
- * @s: character of a string
- * Return: Always 0.
- */
-void _print_rev_recursion(char *s)
-{
-	if (*s != '\0')
-	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	}
-}
-
-*1_print*
-
-#include "main.h"
-
-/**
- * _strlen_recursion - main - check the code
- * @s: character of string
- * Return: Always 0.
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
  */
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-		return (1 + _strlen_recursion(s + 1));
-	}
-	return (0);
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
